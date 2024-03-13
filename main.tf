b988c9bd7a380bca07564c52a6110835e82c9fb1
Michael Lyons <michael.lyonsjhb7@.statefarm.com>
2024-03-13 14:42:14 -0400

    feat: 🎸 usecase -> prefix
    change usecase to prefix to be more generic
    
    BREAKING CHANGE: 🧨 usecase -> prefix

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "iam" {
   source = "./iam"
 
   count                    = var.iam_role_arn == null ? 1 : 0
-  usecase                  = var.usecase
+  prefix                   = var.prefix
   account_name             = var.account_name
   permissions_boundary_arn = var.permissions_boundary_arn
 }
